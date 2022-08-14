c844b0b20783fb2a1b3474a3714350d9bf7f553a
aekryz1993 <ryzaek1993@gmail.com>
2022-08-14 10:52:57 +0100

    update run env

--- src/index.js
+++ src/index.js
@@ -95,20 +95,19 @@ async function main() {
     path: "/graphql",
     cors: {
       credentials: true,
-      origin: "*",
-      // origin: [
-      //   "http://localhost:3000",
-      //   "garmin-clone-aekryz1993.vercel.app",
-      //   "garmin-clone-git-main-aekryz1993.vercel.app",
-      //   "garmin-clone.vercel.app",
-      //   "https://vercel.com/aekryz1993/garmin-clone/BYSrcaFv1R45F8EG9gCif37EyNg2",
-      //   "http://192.168.100.10:3000",
-      //   "https://garmin-clone.vercel.app/",
-      //   "https://garmin-clone-o48qfmlrx-aekryz1993.vercel.app/",
-      //   // "https://garmin-clone-frontend.herokuapp.com/",
-      //   "https://garmin-clone.vercel.app/",
-      //   // "https://studio.apollographql.com",
-      // ],
+      origin: [
+        "http://localhost:3000",
+        "garmin-clone-aekryz1993.vercel.app",
+        "garmin-clone-git-main-aekryz1993.vercel.app",
+        "garmin-clone.vercel.app",
+        "https://vercel.com/aekryz1993/garmin-clone/BYSrcaFv1R45F8EG9gCif37EyNg2",
+        "http://192.168.100.10:3000",
+        "https://garmin-clone.vercel.app/",
+        "https://garmin-clone-o48qfmlrx-aekryz1993.vercel.app/",
+        // "https://garmin-clone-frontend.herokuapp.com/",
+        "https://garmin-clone.vercel.app/",
+        // "https://studio.apollographql.com",
+      ],
     },
   });
 
