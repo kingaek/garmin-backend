cd853c7570917295f1b2db5c40ed19ef9aa93997
aekryz1993 <ryzaek1993@gmail.com>
2022-08-14 11:29:21 +0100

    update run env

--- tools/webpack.common.js
+++ tools/webpack.common.js
@@ -8,7 +8,7 @@ const CURRENT_WORKING_DIR = process.cwd();
 
 const res = (p) => path.resolve(CURRENT_WORKING_DIR, p);
 const entry = res("src/index.js");
-const output = res("public");
+const output = res("dest");
 
 module.exports = merge([
   { name: "server" },
@@ -17,7 +17,7 @@ module.exports = merge([
   { entry: [entry] },
   {
     output: {
-      path: path.resolve(CURRENT_WORKING_DIR, "public"),
+      path: path.resolve(CURRENT_WORKING_DIR, "dest"),
       path: output,
       publicPath: "/",
     },
--- tools/webpack.parts.js
+++ tools/webpack.parts.js
@@ -41,7 +41,7 @@ exports.copyFiles = (currentDir) => ({
       patterns: [
         {
           from: path.resolve(currentDir, "src", "schema.graphql"),
-          to: path.resolve(currentDir, "public"),
+          to: path.resolve(currentDir, "dest"),
         },
       ],
     }),
