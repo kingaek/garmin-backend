549240816985647ccd2df15848ecbd52c36a4615
aekryz1993 <ryzaek1993@gmail.com>
2022-08-16 15:09:09 +0100

    add origins

--- src/index.js
+++ src/index.js
@@ -99,6 +99,7 @@ async function main() {
       origin: [
         "http://localhost:3000",
         "https://garmin-clone.netlify.app",
+        "https://garmin-clone.herokuapp.com",
         "https://62f8c9ae94aca554c7cf5d75--friendly-fox-a3e655.netlify.app",
         "garmin-clone-aekryz1993.vercel.app",
         "garmin-clone-git-main-aekryz1993.vercel.app",
